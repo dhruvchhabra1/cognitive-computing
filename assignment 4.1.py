75dec6b933f124df6c1e863d0734b0dbe252f7e4
dhruvchhabra1 <dchhabra_be24@thapar.edu>
2026-07-28 16:41:42 +0530

    Update assignment 4.1.py

--- assignment 4.1.py
+++ assignment 4.1.py
@@ -1,9 +1,9 @@
 #table of 7
 print("table of 7:")
-for i in range (1,10):
+for i in range (1,11):
   print(7,"*",i,"=",7*i)
 
 print("table of 9")
 #table of 9
-for i in range(1,10):
+for i in range(1,11):
   print(9,"*",i,"=",9*i)
